393b5d7dfa9f917398977c58747ee38be042650f
Ferrad MAKOSSO <ferrad.fm@gmail.com>
2020-03-07 14:19:34 +0100

    new changes

--- cityRoutes.js
+++ cityRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const cityController = require("./cityController");
 const authController = require("./authController");
 
-router.route("/").get(authController.protect, cityController.getAll);
-router.route("/:id").get(authController.protect, cityController.getCity);
-router.route("/:id").put(authController.protect, cityController.update);
-router.route("/:id").delete(authController.protect, cityController.delete);
+router.route("/").get(cityController.getAll);
+router.route("/:id").get(cityController.getCity);
+router.route("/:id").put(cityController.update);
+router.route("/:id").delete(cityController.delete);
 
 module.exports = router;
